a7c36110850acd1f3e2d33c966f33567b2593395
Oscar Gutierrez <oscarfgutierrezo@gmail.com>
2022-05-18 12:17:17 -0500

    Add: Message and function for reset form after successful subscription

--- public/js/mailValidation.js
+++ public/js/mailValidation.js
@@ -1,16 +1,21 @@
-const subscribeContainer = document.querySelector("#subscribeContainer")
-const subscribeInput =document.querySelector("#subscribeInput")
+const subscribeContainer = document.querySelector("#subscribeContainer");
+const subscribeForm = document.querySelector("#subscribeForm");
+const subscribeInput =document.querySelector("#subscribeInput");
 const subscribeBtn = document.querySelector("#subscribeBtn");
 const er = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-document.addEventListener("DOMContentLoaded", disableBtn);
+// Desabilitar el botón "Subscribe" al cargar la página
 
-subscribeInput.addEventListener("blur", validateEmail)
+document.addEventListener("DOMContentLoaded", disableBtn);
 
 function disableBtn() {
     subscribeBtn.disabled = true;
     subscribeBtn.classList.add("disableBtn");
 }
+
+// Validar el email
+
+subscribeInput.addEventListener("blur", validateEmail)
 
 function validateEmail(evt) {
     cleanErrors();
@@ -39,3 +44,26 @@ function cleanErrors() {
         error.remove()
     }
 }
+
+// Mostrar mensaje para confirmar la subscripción
+
+subscribeForm.addEventListener("submit", enviarEmail);
+
+function enviarEmail(evt) {
+    evt.preventDefault();
+    const sendMessage = document.createElement("p");
+    sendMessage.classList.add("sendMessage")
+    sendMessage.textContent = "You have successfully subscribed"
+    subscribeContainer.insertBefore(sendMessage, subscribeContainer.children[3]);
+    setTimeout(() => {
+        sendMessage.remove();
+        resetForm();    
+    }, 3000);
+}
+
+function resetForm() {
+    subscribeForm.reset();
+    disableBtn();
+}
+
+
